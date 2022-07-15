3fca17f7dada3cf7a1ea2e70326074c206777c27
Marco Braccini <marco.braccini@live.it>
2022-07-15 17:31:12 +0200

    Working on program structure

--- js/main.js
+++ js/main.js
@@ -1,33 +1,55 @@
+let loading = document.getElementById('loading');
+let startBtn = document.getElementById('start-btn');
+let gameContainer = document.getElementById('game-container');
+
+setTimeout(function() {
+
+    loading.style.display = 'none';
+    startBtn.style.display = 'block';
+
+}, 1000);
+
+startBtn.addEventListener('click', function() {
+
+    startBtn.style.display = 'none';
+    gameContainer.style.display = 'block';
+
+});
+
+let randomNumberArray = [];
+randomUniqueNumber(5, randomNumberArray, 1, 99);
+
+console.log(randomNumberArray);
 
 
 
 
 
-/*function randomUniqueNumber(maxEndElement, min, max) {
-    let numberEndGame;
+function randomUniqueNumber(maxNumberList, numberListName, min, max) {
+    let numberList;
     let numberVerify = false;
 
-    for (let list = 0; list < maxEndElement; list++) {
-        numberEndGame = randomNumber(min, max);
+    for (let list = 0; list < maxNumberList; list++) {
+        numberList = randomNumber(min, max);
         
-        if (randomUniqueNumberList.includes(numberEndGame)) {
+        if (numberListName.includes(numberList)) {
             numberVerify = true;
         }
         
         while (numberVerify == true) {
-            numberEndGame = randomNumber(min, max);
+            numberList = randomNumber(min, max);
 
-            if (!randomUniqueNumberList.includes(numberEndGame)) {
+            if (!numberListName.includes(numberList)) {
                 numberVerify = false;
             }
         }
         
-        randomUniqueNumberList.push(numberEndGame);
+        numberListName.push(numberList);
         }
 }
 
 function randomNumber(min, max) {
     const number = Math.floor(Math.random() * (max - min + 1) + min);
-
+    
     return number;
-}*/
+}
