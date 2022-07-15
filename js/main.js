7953aa2f938473effaa6d250becda10da6a06d30
Marco Braccini <marco.braccini@live.it>
2022-07-15 19:02:20 +0200

    Add js countdown - Add js number verify

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let loading = document.getElementById('loading');
 let startBtn = document.getElementById('start-btn');
 let gameContainer = document.getElementById('game-container');
+const randomList = document.getElementById('random-number');
+const verifyBtn = document.getElementById('verify-btn');
 
 setTimeout(function() {
 
@@ -16,10 +18,59 @@ startBtn.addEventListener('click', function() {
 
 });
 
+
+let clock = undefined;
+let timer = 5;
+if (clock == undefined && timer == 5) {
+    clock = setInterval(function() {
+
+    timer -= 1;
+
+    console.log(timer);
+
+    if (timer == 0) {
+        clearInterval(clock);
+        clock = undefined;
+    }
+    }, 1000);
+}
+
+
+
+
+
+
 let randomNumberArray = [];
 randomUniqueNumber(5, randomNumberArray, 1, 99);
 
-console.log(randomNumberArray);
+console.log(randomNumberArray)
+
+for (let i = 0; i < randomNumberArray.length; i++) {
+    let numberContainer = document.createElement('li');
+    numberContainer.append(randomNumberArray[i]);
+    randomList.append(numberContainer);
+}
+
+const inputList = document.querySelectorAll('.input-number');
+
+verifyBtn.addEventListener('click', function() {
+
+    let checkNumber = false;
+    let counterTrue = 0;
+    for (let i = 0; i < inputList.length; i++) {
+        console.log(inputList[i].value)
+        if (randomNumberArray.includes(parseInt(inputList[i].value))) {
+            checkNumber = true;
+            counterTrue++;
+        }
+    }
+    if (checkNumber == true) {
+        console.log('ciaooo')
+        console.log(counterTrue)
+    } else {
+        console.log('nooo')
+    }
+});
 
 
 
