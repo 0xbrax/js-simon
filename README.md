7aa89486f340c43eaeedc86f95a69c0431315ee2
Marco Braccini <marco.braccini@live.it>
2022-07-15 17:06:26 +0200

    @INIT Project Add readme workflow

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# js-simon
+Create loading screen with timer
+Create start game button to show the game
+
+The game appears
+Generate 5 unique random numbers
+
+30 seconds timer for:
+- Make the 5 numbers disappear
+- Try to enter the number
+- Show result about how many numbers have been guessed
---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+/*function randomUniqueNumber(maxEndElement, min, max) {
+    let numberEndGame;
+    let numberVerify = false;
+
+    for (let list = 0; list < maxEndElement; list++) {
+        numberEndGame = randomNumber(min, max);
+        
+        if (randomUniqueNumberList.includes(numberEndGame)) {
+            numberVerify = true;
+        }
+        
+        while (numberVerify == true) {
+            numberEndGame = randomNumber(min, max);
+
+            if (!randomUniqueNumberList.includes(numberEndGame)) {
+                numberVerify = false;
+            }
+        }
+        
+        randomUniqueNumberList.push(numberEndGame);
+        }
+}
+
+function randomNumber(min, max) {
+    const number = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return number;
+}*/
